57fed7bac810e5e0edfa7c15fa517e0eea4998a2
Travis Unruh <travis.unruh@gmail.com>
2023-04-10 00:22:46 -0500

    Piece tests

--- Stratego.Tests/PieceTests.cs
+++ Stratego.Tests/PieceTests.cs
@@ -13,6 +13,16 @@ public class PieceTests
     }
 
     [Fact]
+    public void show_empty_piece_selected()
+    {
+        var piece = new Piece();
+
+        piece.SetType(PieceType.Empty);
+
+        Assert.Equal(">.<", piece.Display(true));
+    }
+
+    [Fact]
     public void no_mans_land_piece()
     {
         var piece = new Piece();
@@ -20,5 +30,45 @@ public class PieceTests
         piece.SetType(PieceType.No_Mans_Land);
 
         Assert.Equal("###", piece.Display());
+    }
+
+    [Fact]
+    public void display_bomb()
+    {
+        var piece = new Piece();
+
+        piece.SetType(PieceType.Bomb);
+
+        Assert.Equal(" B ", piece.Display());
+    }
+
+    [Fact]
+    public void display_spy()
+    {
+        var piece = new Piece();
+
+        piece.SetType(PieceType.Spy);
+
+        Assert.Equal(" S ", piece.Display());
+    }
+
+    [Fact]
+    public void display_flag()
+    {
+        var piece = new Piece();
+
+        piece.SetType(PieceType.Flag);
+
+        Assert.Equal("<F>", piece.Display());
+    }
+
+    [Fact]
+    public void display_one()
+    {
+        var piece = new Piece();
+
+        piece.SetType(PieceType.One);
+
+        Assert.Equal(" 1 ", piece.Display());
     }
 }
--- Stratego/Piece.cs
+++ Stratego/Piece.cs
@@ -2,12 +2,42 @@ namespace Stratego;
 
 public class Piece
 {
-    PieceType _type = PieceType.Empty;
-    public string Display()
+    PieceType _type;
+
+    public Piece()
     {
-        var display = " . ";
+        _type = PieceType.Empty;
+    }
+
+    public Piece(PieceType type)
+    {
+        _type = type;
+    }
+    public string Display(bool isSelected = false)
+    {
+        string leftFiller = isSelected ? ">" :" ";
+        string rightFiller = isSelected ? "<" :" ";
+
+        var display = $"{leftFiller}.{rightFiller}";
         if(_type == PieceType.No_Mans_Land)
             display = "###";
+        else if(_type == PieceType.Spy)
+            display = $"{leftFiller}S{rightFiller}";
+        else if(_type == PieceType.Bomb)
+            display = $"{leftFiller}B{rightFiller}";
+        else if(_type == PieceType.Flag)
+            display = "<F>";
+        else if(_type == PieceType.One
+                || _type == PieceType.Two
+                || _type == PieceType.Three
+                || _type == PieceType.Four
+                || _type == PieceType.Five
+                || _type == PieceType.Six
+                || _type == PieceType.Seven
+                || _type == PieceType.Eight
+                || _type == PieceType.Nine
+                || _type == PieceType.Ten)
+            display = $"{leftFiller}{(int) _type}{rightFiller}";
         return display;
     }
 
